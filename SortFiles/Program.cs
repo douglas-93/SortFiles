4819056d7ef10fc95418c0e59ddc9d867b0b0596
Douglas Andrade <dolts93@outlook.com>
2021-10-05 17:09:04 -0300

    Adicionar arquivos de projeto.

---
+++ SortFiles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace SortFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userName = System.Environment.UserName;
+            var dir = $"C:\\Users\\{userName}\\Downloads\\";
+
+            var directory = Directory.GetDirectories(dir);
+            var files = Directory.GetFiles(dir);
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                var fileName = fileInfo.Name;
+                var lastWriteTime = fileInfo.LastWriteTime.ToString().Split(" ")[0];
+                lastWriteTime = lastWriteTime.Replace("/", "_");
+
+                if (!Directory.Exists(Path.Combine(Path.GetDirectoryName(file), lastWriteTime)))
+                {
+                    Directory.CreateDirectory(Path.Combine(Path.GetDirectoryName(file), lastWriteTime));
+                }
+                File.Move(file, Path.Combine(Path.GetDirectoryName(file), lastWriteTime, fileName));
+            }
+        }
+    }
+}
